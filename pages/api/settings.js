2aeccee6d99c589158adf15c5f0bc776f30dc773
abdalrahmanabnmohammed-png <abdalrahman.abn.mohammed@gmail.com>
2026-01-15 13:41:35 +0300

    Update settings.js

--- pages/api/settings.js
+++ pages/api/settings.js
@@ -3,8 +3,9 @@ import mongoose from 'mongoose';
 
 const SettingSchema = new mongoose.Schema({
   guildId: { type: String, default: 'default' },
-  infoTitle: String, infoDescription: String, infoImage: String, infoThumbnail: String, infoColor: String,
-  ticketCategory: String, ticketSupportRole: String, ticketReasons: Array
+  infoTitle: String, infoDescription: String, infoImage: String, infoColor: String,
+  ticketCategory: String, ticketSupportRole: String, ticketReasons: Array,
+  logChannel: String // التأكد من إضافة الحقل هنا
 });
 
 const Setting = mongoose.models.Setting || mongoose.model('Setting', SettingSchema);
